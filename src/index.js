78d6ee8a403ef9e8d22d8f9ff7faa3dfd4919ee9
dirghayu <dirghayu.kaninde@gmail.com>
2017-03-14 11:26:04 +1100

    The final delivery of product. React rocks :)

--- src/index.js
+++ src/index.js
@@ -5,7 +5,9 @@ import SearchBar from './components/search_bar'
 import VideoList from './components/video_list'
 import VideoDetail from './components/video_details'
 import YTSearch from 'youtube-api-search'
+import _ from 'lodash';
 const API_KEY = 'AIzaSyDjDjpPAobAlpnK_p883w7cZ9mV6Ub6GAg';
+
 
 class App extends Component {
 
@@ -16,17 +18,23 @@ class App extends Component {
             videos: [],
             selectedVideo: null
         };
+        this.videoSearch("Yoga");
 
-    YTSearch({key: API_KEY, term: 'yoga'},(videos)=>{
-      this.setState({
-      videos: videos,
-      selectedVideo : videos[0]});
-   });
+    }
+
+    videoSearch(term) {
+        YTSearch({key: API_KEY, term: term},(videos)=>{
+          this.setState({
+          videos: videos,
+          selectedVideo : videos[0]});
+       });
+
     }
 
     render(){
+    const videoSearch = _.debounce((term)=> {this.videoSearch(term)},3000 );
         return <div>
-            <SearchBar />
+            <SearchBar onSearchTermChange={videoSearch} />
             <VideoDetail video={this.state.selectedVideo}/>
             <VideoList videos={this.state.videos} onVideoSelect={selectedVideo=> this.setState({selectedVideo})}/>
         </div>;
